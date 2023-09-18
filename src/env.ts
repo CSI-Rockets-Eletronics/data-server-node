b316ce0b0b82ca2381a1ed250b6ffd3891077342
Alex Chen <axchen7@outlook.com>
2023-09-17 20:18:05 -0400

    Add env

---
+++ src/env.ts
@@ -0,0 +1,14 @@
+/* eslint-disable @typescript-eslint/naming-convention */
+
+import process from 'node:process';
+import {z} from 'zod';
+import 'dotenv/config.js'; // eslint-disable-line import/no-unassigned-import
+
+const envSchema = z.object({
+	PORT: z.string().transform(Number),
+	NODE_NAME: z.string(),
+	IS_SESSION_MAKER: z.string().transform((value) => value === 'true'),
+	DATABASE_URL: z.string(),
+});
+
+export const env = envSchema.parse(process.env);
