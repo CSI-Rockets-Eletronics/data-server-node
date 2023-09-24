5eb59b3c8424e18838ab32ab731b23e81ed95d8d
Alex Chen <axchen7@outlook.com>
2023-09-23 20:42:26 -0400

    Correctly estimate path createdAt

--- src/routes/records.ts
+++ src/routes/records.ts
@@ -4,6 +4,8 @@ import {prisma} from '../prisma';
 export const recordsRoute = new Elysia({prefix: '/records'}).post(
 	'',
 	async ({body}) => {
+		const pathCreatedAtMs = Date.now() - body.ts / 1000;
+
 		await prisma.record.upsert({
 			where: {
 				environmentKey_path_ts: {
@@ -24,7 +26,7 @@ export const recordsRoute = new Elysia({prefix: '/records'}).post(
 						create: {
 							environmentKey: body.environmentKey,
 							path: body.path,
-							createdAt: new Date(),
+							createdAt: new Date(pathCreatedAtMs),
 						},
 					},
 				},
