fa805acdbffb5114a30d34782899912011049ba9
Alex Chen <axchen7@outlook.com>
2023-10-17 01:45:54 -0400

    Fix fetch patch

--- src/api.ts
+++ src/api.ts
@@ -9,6 +9,8 @@ export const createEdenTreaty = (origin: string) => {
 	// EdenTreaty has a bug where it will include undefined query parameters in
 	// the URL, so we patch fetch to remove them.
 
+	// NOTE: THIS MAY MESS WITH STRING QUERY PARAMS THAT ARE LITERALLY 'undefined'
+
 	const originalFetch = fetch;
 
 	// @ts-expect-error overwrite fetch
@@ -20,7 +22,7 @@ export const createEdenTreaty = (origin: string) => {
 			const url = new URL(request);
 
 			for (const [key, value] of url.searchParams.entries()) {
-				if (value === undefined) {
+				if (value === 'undefined') {
 					url.searchParams.delete(key);
 				}
 			}
