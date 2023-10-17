dbf7de4da671ee04b629e356449ef439434dedcb
Alex Chen <axchen7@outlook.com>
2023-10-17 00:37:54 -0400

    Patch edenTreaty

--- src/api.ts
+++ src/api.ts
@@ -2,6 +2,32 @@ import {edenTreaty} from '@elysiajs/eden/treaty';
 import {type App} from '.';
 
 /**
+ * Ensures that undefined keys are stripped, not stringified. E.g. `new
+ * URLSearchParams({foo: undefined})` should be `''`, not `'foo=undefined'`.
+ * This is necessary because edenTreaty uses URLSearchParams internally.
+ */
+class FixedURLSearchParameters extends URLSearchParams {
+	constructor(
+		init?: string[][] | Record<string, string> | string | URLSearchParams,
+	) {
+		if (
+			typeof init === 'object' &&
+			!Array.isArray(init) &&
+			!(init instanceof URLSearchParams)
+		) {
+			init = Object.fromEntries(
+				Object.entries(init).filter(([, value]) => value !== undefined),
+			);
+		}
+
+		super(init);
+	}
+}
+
+// eslint-disable-next-line no-global-assign
+URLSearchParams = FixedURLSearchParameters as any;
+
+/**
  * Other packages that use this package as a dependency can use this function to
  * make API calls.
  */
