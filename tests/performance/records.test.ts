0cc8adbce2bff9c3abd4ec1426d62fdd2e15ef7c
Alex Chen <axchen7@outlook.com>
2023-11-25 13:16:57 -0800

    Fix performance/records.test

--- tests/performance/records.test.ts
+++ tests/performance/records.test.ts
@@ -3,24 +3,34 @@ import {describe, expect, test} from 'bun:test';
 import {testNode} from '../test-node';
 import {catchError} from '../helpers';
 import {environmentKey, environmentKey2} from '../setup';
+import {curTimeMicros} from '../../src/helpers';
+
+function createManyRecords() {
+	const firstTs = curTimeMicros();
+	const manyRecords: Array<{ts: number; data: unknown}> = [];
+
+	for (let i = 0; i < 5000; i++) {
+		manyRecords.push({
+			ts: firstTs + i,
+			data: {bar: 'baz'},
+		});
+	}
+
+	return manyRecords;
+}
 
 describe('/records', () => {
 	test('upload and get many records', async () => {
-		const manyRecords: Array<{ts: number; data: unknown}> = [];
-		for (let ts = 0; ts < 5000; ts++) {
-			manyRecords.push({ts, data: {bar: 'baz'}});
-		}
-
 		for (const key of [environmentKey, environmentKey2]) {
 			for (let session = 0; session < 5; session++) {
 				await catchError(testNode.sessions.create.post({environmentKey: key}));
 
-				for (let path = 0; path < 5; path++) {
+				for (let device = 0; device < 5; device++) {
 					await catchError(
 						testNode.records.batch.post({
 							environmentKey,
-							path: `foo${path}`,
-							records: manyRecords,
+							device: `foo${device}`,
+							records: createManyRecords(),
 						}),
 					);
 				}
@@ -31,7 +41,7 @@ describe('/records', () => {
 			testNode.records.get({
 				$query: {
 					environmentKey,
-					path: 'foo0',
+					device: 'foo0',
 					startTs: '0', // Get earliest records first
 				},
 			}),
@@ -47,7 +57,7 @@ describe('/records', () => {
 				testNode.records.get({
 					$query: {
 						environmentKey,
-						path: 'foo0',
+						device: 'foo0',
 						startTs: Math.round(minTs + tsDiff / 4).toString(),
 						endTs: Math.round(maxTs - tsDiff / 4).toString(),
 					},
