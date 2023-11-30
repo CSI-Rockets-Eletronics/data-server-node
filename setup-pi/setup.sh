7dbf8c5271c7e27e8d3242c17d5839c6385e0d5e
Alex Chen <axchen7@outlook.com>
2023-11-29 20:34:18 -0500

    Add setup-pi

---
+++ setup-pi/setup.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+# Install PostgreSQL
+sudo apt-get install -y postgresql
+
+# Copy pg_hba.conf and postgresql.conf to the PostgreSQL config folder
+cd "$(dirname "$0")"
+sudo cp pg_hba.conf /etc/postgresql/15/main/
+sudo cp postgresql.conf /etc/postgresql/15/main/
+
+# Restart PostgreSQL to apply the new config
+sudo systemctl restart postgresql
+
+# Install bun
+curl -fsSL https://bun.sh/install | bash
+
+# Install Node.js (necessary for Prisma)
+sudo apt-get install -y nodejs
+
+# Install dependencies and generate Prisma client
+cd "$(dirname "$0")/.."
+bun install
+bun prisma:generate
+
+# Copy the service file to the systemd services folder
+cd "$(dirname "$0")"
+sudo cp data-server-node.service /etc/systemd/system/
+
+# Reload systemd daemon to read the new service file
+sudo systemctl daemon-reload
+
+# Enable the service to run on boot
+sudo systemctl enable data-server-node.service
+
+# Start the service
+sudo systemctl start data-server-node.service
